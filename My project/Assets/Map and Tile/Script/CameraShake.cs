988aba52ce8d9ba97c3c00307262a394010ea8d4
hmh4947 <hmh4947@naver.com>
2023-08-27 23:02:32 +0900

    Update CameraShake
    shake시 camera 범위 설정, 스크립트를 불러올 수 있는 예시 스크립트 작성

--- My project/Assets/Map and Tile/Script/CameraShake.cs
+++ My project/Assets/Map and Tile/Script/CameraShake.cs
@@ -8,30 +8,50 @@ public class CameraShake : MonoBehaviour
    
     private float shakeTime;    // 흔들림 지속 시간  
     private float shakeIntensity;   // 흔들림 세기  
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
 
-    private void Update()
+    private static CameraShake instance;
+    public static CameraShake Instance=>instance;
+
+  /*  private void Update()
     {
         if (Input.GetKeyDown("1"))
         {
             OnShakeCamera(0.1f, 1f);
         }
-    }
-
-   public void OnShakeCamera(float shakeTime=1.0f, float shakeIntensity=1.0f)
+    }*/
+  public CameraShake()
     {
+        instance = this;
+    }
+   public void OnShakeCamera(float shakeTime=0.5f, float shakeIntensity=0.3f)
+    {
+
+       
         this.shakeTime = shakeTime;
         this.shakeIntensity = shakeIntensity;
         StopCoroutine("ShakeByPosition");
         StartCoroutine("ShakeByPosition");
+        instance = this;    
     }
     private IEnumerator ShakeByPosition()
     {
         Vector3 startPosition=transform.position;
-        while(shakeTime > 0f) 
+        Vector3 originalPosition = startPosition;
+        while (shakeTime > 0f) 
         {
-            transform.position = startPosition + Random.insideUnitSphere * shakeIntensity;
+            Vector3 shakeOffset = Random.insideUnitSphere * shakeIntensity;
+            Vector3 newPosition = originalPosition + shakeOffset;
+
+            newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+            newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
+            transform.position = newPosition;
             shakeTime -= Time.deltaTime;
             yield return null;
+            
         }
         transform.position = startPosition;
     }
